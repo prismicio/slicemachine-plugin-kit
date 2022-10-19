ec0cb1689c7b3b21aa18b0da0baaae3bc7339cdc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-10-19 12:58:34 -1000

    test: add test for `readAllSliceModels()` with metadata

--- test/SliceMachineActions-readAllSliceModels.test.ts
+++ test/SliceMachineActions-readAllSliceModels.test.ts
@@ -72,6 +72,82 @@ it("returns all slice models from all libraries", async (ctx) => {
 	expect(res).toStrictEqual(libraries.flatMap((library) => library.__models));
 });
 
+it("returns Slices with metadata if requested", async (ctx) => {
+	const library1Models = [
+		ctx.mock.model.sharedSlice(),
+		ctx.mock.model.sharedSlice(),
+	];
+	const library2Models = [
+		ctx.mock.model.sharedSlice(),
+		ctx.mock.model.sharedSlice(),
+	];
+	const libraries = [
+		{
+			id: "lib-1",
+			sliceIDs: library1Models.map((model) => model.id),
+			__models: library1Models,
+		},
+		{
+			id: "lib-2",
+			sliceIDs: library2Models.map((model) => model.id),
+			__models: library2Models,
+		},
+	];
+
+	let actions!: SliceMachineActions;
+
+	const adapter = createTestAdapter({
+		setup: ({ hook }) => {
+			hook("__debug__", async (_, context) => {
+				actions = context.actions;
+			});
+			hook("slice-library:read", async (args) => {
+				const library = libraries.find((library) => {
+					return library.id === args.libraryID;
+				});
+
+				if (library) {
+					return library;
+				}
+
+				throw new Error("not implemented");
+			});
+			hook("slice:read", async (args) => {
+				const library = libraries.find(
+					(library) => library.id === args.libraryID,
+				);
+				const model = library?.__models.find(
+					(model) => model.id === args.sliceID,
+				);
+
+				if (model) {
+					return model;
+				}
+
+				throw new Error("not implemented");
+			});
+		},
+	});
+	const project = createSliceMachineProject(adapter);
+	project.config.libraries = libraries.map((library) => library.id);
+
+	const pluginRunner = createSliceMachinePluginRunner({ project });
+	await pluginRunner.init();
+	await pluginRunner.callHook("__debug__", undefined);
+
+	const res = await actions.readAllSliceModels({ withMetadata: true });
+	expect(res).toStrictEqual(
+		libraries.flatMap((library) => {
+			return library.__models.map((model) => {
+				return {
+					libraryID: library.id,
+					model,
+				};
+			});
+		}),
+	);
+});
+
 it("returns empty array when project has no Slice Libraries", async () => {
 	let actions!: SliceMachineActions;
 
