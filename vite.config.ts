54484de2af6f9a309eadc5b74626dd12e2ac1999
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-11-08 16:40:11 -1000

    fix: treat `strip-indent` as an internal dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import sdk from "vite-plugin-sdk";
 
 export default defineConfig({
-	plugins: [sdk()],
+	plugins: [
+		sdk({
+			internalDependencies: ["strip-indent"],
+		}),
+	],
 	test: {
 		coverage: {
 			provider: "c8",
